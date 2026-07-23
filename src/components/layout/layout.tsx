416fd7a6e87816c543e16d200e3e12afacb810fc
Erivaldo Manuel <erivaldomanuelz35@gmail.com>
2026-07-23 14:46:31 +0100

    alias configurate at project

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
-import Navbar from '../navbar/navbar'
-import Footer from '../footer/footer'
+import Navbar from '@/components/navbar/navbar'
+import Footer from '@/components/footer/footer'
 
 function Layout() {
   return (
